737e5ad03d2ce3857139d6cd1da771f426ec45fa
Yuri3358 <yurialmeida917@gmail.com>
2023-06-17 10:59:23 -0300

    feat(#infos): data field

--- templates/calculatorTemplate.js
+++ templates/calculatorTemplate.js
@@ -1,11 +1,18 @@
 const calculatorTemplate = `
-    <section id="calculator">
-        <label class="labels" for="weight-input">Peso (kg):</label>
-        <input class="inputs" id="weight-input"> <br/>
+    <section id="calculator" @keyup.enter="getInfos">
+        <label class="labels" for="height-input">Altura (cm):</label>
+        <input class="inputs" type="number" id="height-input" v-model="height" ref="weightInput" min="1">
+        <span class="units">cm</span> <br/>
 
-        <label class="labels" for="height-input">Altura (m):</label>
-        <input class="inputs" id="height-input"> <br/>
-
-        <button class="btn btn-success">Calcular IMC</button>
+        <label class="labels" for="weight-input">Peso (Kg):</label>
+        <input class="inputs" type="number" id="weight-input" v-model="weight" min="1">
+        <span class="units">Kg</span> <br/>
+        
+        <button class="btn btn-success" @click="getInfos">Calcular IMC</button>
+        <hr>
+        <div id="infos">
+            {{ infos }}
+            {{ bmiRate }}
+        </div>
     </section>
 `
