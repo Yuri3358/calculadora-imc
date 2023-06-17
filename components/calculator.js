49ab7a2a87a4957175ad11f586d8590eb29ac00e
Yuri3358 <yurialmeida917@gmail.com>
2023-06-17 10:58:40 -0300

    feat: bmi rate

--- components/calculator.js
+++ components/calculator.js
@@ -5,13 +5,30 @@ const calculator = {
             weight: 0,
             height: 0,
             bmi: 0,
+            bmiRate: "",
             infos: "" 
         }
     },
     methods: {
         getInfos() {
-            this.bmi = (this.weight / this.height**2)*10000
-            this.infos = `Seu IMC: ${this.bmi.toFixed(2)}kg/cm²`
+            if (this.weight == 1 || this.height == 1) {
+                alert("Preencha todos os valores!")
+            } else {
+                this.bmi = (this.weight / this.height**2)*10000
+                this.infos = `Seu IMC: ${this.bmi.toFixed(2)}kg/cm²`
+    
+                if (this.bmi < 16) {
+                    this.bmiRate = "Baixo Peso"
+                } else if (this.bmi >= 18 && this.bmi < 25) {
+                    this.bmiRate = "Adequado"
+                } else if (this.bmi >= 25 && this.bmi < 30) {
+                    this.bmiRate = "Sobrepeso"
+                } else if (this.bmi >= 30) {
+                    this.bmiRate = "Obesidade"
+                }
+
+                this.$refs.weightInput.focus()
+            }
         }
     }
 }
