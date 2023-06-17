1bdefaec38b13f534c6e09cf11859bb5f873df7d
Yuri3358 <yurialmeida917@gmail.com>
2023-06-17 10:43:11 -0300

    feat: bmi calculator

--- components/calculator.js
+++ components/calculator.js
@@ -1,5 +1,19 @@
 const calculator = {
-    template: calculatorTemplate
+    template: calculatorTemplate,
+    data() {
+        return {
+            weight: 0,
+            height: 0,
+            bmi: 0,
+            infos: "" 
+        }
+    },
+    methods: {
+        getInfos() {
+            this.bmi = (this.weight / this.height**2)*10000
+            this.infos = `Seu IMC: ${this.bmi.toFixed(2)}kg/cm²`
+        }
+    }
 }
 
 exports = { calculator }
